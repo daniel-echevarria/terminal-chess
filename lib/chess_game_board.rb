997ace19c399e2566d625bf84ca80a0cd5617398
Kasper <kasperbak89@pm.me>
2023-05-25 23:35:49 +0200

    Implement a display_board method

--- lib/chess_game_board.rb
+++ lib/chess_game_board.rb
@@ -9,4 +9,17 @@ class ChessBoard
   def create_board
     Array.new(8) { Array.new(8) }
   end
+
+  def display_board
+    i = 8
+    @board.each do |row|
+      puts " #{i}  #{row.join(' | ')} "
+      puts '   -----------------------'
+      i -= 1
+    end
+  end
 end
+
+chess_board = ChessBoard.new
+
+chess_board.display_board
