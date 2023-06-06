34401cfd0bde6f8d9fa38b22b9ba147edcabba70
Kasper <kasperbak89@pm.me>
2023-06-06 20:14:28 +0200

    Add ChessPiece class as a basis for the actual pieces

---
+++ lib/chess_game_piece.rb
@@ -0,0 +1,12 @@
+
+
+class ChessPiece
+
+  attr_accessor :position, :color, :unicode
+
+  def initialize(position, color)
+    @position = position
+    @color = color
+    @unicode = set_unicode
+  end
+end
