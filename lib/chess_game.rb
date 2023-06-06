4e17ea8c7b7de589109834cc8f565a37182845c8
Kasper <kasperbak89@pm.me>
2023-06-06 12:56:36 +0200

    Refactor: Start improving class responsibility sharing by moving components

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -36,23 +36,6 @@ class ChessGame
   def game_over?
   end
 
-  def snack_piece_at(position)
-    piece = select_piece_at(position)
-    @pieces.delete(piece)
-  end
-
-  # given a position and a piece list all the possible moves
-
-  def move_piece(piece, position)
-    snack_piece_at(position) if has_oponent(piece, position)
-    piece.position = position
-  end
-
-  def select_piece_at(position)
-    piece = @pieces.select { |piece| piece.position == position }
-    piece.first
-  end
-
   def select_piece_input(player)
     select_piece_message(player)
     input = gets.chomp
@@ -85,78 +68,6 @@ class ChessGame
     array_position = translate_chess_to_array(chess_position)
     possible_moves = get_potential_moves(piece)
     possible_moves.include?(array_position)
-  end
-
-  # Get info about a position
-
-  def has_oponent(moving_piece, position)
-    piece = select_piece_at(position)
-    return false if piece.nil?
-
-    piece.color != moving_piece.color
-  end
-
-  def has_ally(moving_piece, position)
-    piece = select_piece_at(position)
-    return false if piece.nil?
-
-    piece.color == moving_piece.color
-  end
-
-  def is_out_of_board?(position)
-    row, col = position
-    row.between?(0, 7) && col.between?(0, 7) ? false : true
-  end
-
-  def generate_up_vertical_moves(piece, next_move = move_vertically(piece.position, -1), moves = [])
-    return moves if has_ally(piece, next_move)
-    moves << next_move and return moves if has_oponent(piece, next_move) || is_out_of_board?(next_move)
-
-    moves << next_move
-    next_move = move_vertically(next_move, -1)
-    generate_up_vertical_moves(piece, next_move, moves)
-  end
-
-  def generate_down_vertical_moves(piece, next_move = move_vertically(piece.position, 1), moves = [])
-    return moves if has_ally(piece, next_move)
-    moves << next_move and return moves if has_oponent(piece, next_move) || is_out_of_board?(next_move)
-
-    moves << next_move
-    next_move = move_vertically(next_move, 1)
-    generate_down_vertical_moves(piece, next_move, moves)
-  end
-
-  def generate_left_horizontal_moves(piece, next_move = move_horizontally(piece.position, -1), moves = [])
-    return moves if has_ally(piece, next_move)
-    moves << next_move and return moves if has_oponent(piece, next_move) || is_out_of_board?(next_move)
-
-    moves << next_move
-    next_move = move_horizontally(next_move, -1)
-    generate_left_horizontal_moves(piece, next_move, moves)
-  end
-
-  def get_potential_moves(piece)
-    moves = []
-    moves << piece.potential_moves
-    remove_invalid_moves(piece, moves)
-    moves << special_case_pawn_moves(piece) if piece.is_a?(ChessPawn)
-    moves.flatten(1)
-  end
-
-  def remove_invalid_moves(piece, moves)
-    moves.delete_if { |pos| has_ally(piece, pos) }
-  end
-
-  def special_case_pawn_moves(pawn)
-    direction = pawn.color == 'white' ? 1 : -1
-    main_diag_move = move_main_diagonal(pawn.position, direction)
-    sec_diag_move = move_secondary_diagonal(pawn.position, direction)
-
-    valid_moves = []
-    valid_moves << main_diag_move if has_oponent(pawn, main_diag_move)
-    valid_moves << sec_diag_move if has_oponent(pawn, sec_diag_move)
-
-    valid_moves
   end
 
   def translate_chess_to_array(input)
--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -11,7 +11,6 @@ describe ChessGame do
   subject(:game) { described_class.new(board, player_1, player_2) }
 
   describe "#select_piece_input" do
-
     context 'when player inputs a valid position' do
 
       before do
@@ -165,44 +164,6 @@ describe ChessGame do
         input = 'h8'
         result = game.translate_chess_to_array(input)
         expect(result).to eq([0, 7])
-      end
-    end
-  end
-
-  describe '#select_piece_at' do
-    let(:pawn_1) { instance_double(ChessPawn, position: [6, 0]) }
-    let(:pawn_2) { instance_double(ChessPawn, position: [2, 3]) }
-    let(:pawn_3) { instance_double(ChessPawn, position: [5, 0]) }
-
-    before do
-      game.instance_variable_set(:@pieces, [pawn_1, pawn_2, pawn_3])
-    end
-
-    context 'when there is a piece on the selected position' do
-      context 'when selected position is [6, 0]' do
-        it 'returns the piece on [6, 0]' do
-          position = [6, 0]
-          result = game.select_piece_at(position)
-          expect(result).to eq(pawn_1)
-        end
-      end
-
-      context 'when selected position is [2, 3]' do
-        it 'returns the piece on [2, 3]' do
-          position = [2, 3]
-          result = game.select_piece_at(position)
-          expect(result).to eq(pawn_2)
-        end
-      end
-    end
-
-    context 'when there is no piece on the selected position' do
-      context 'when selected position is [4, 2]' do
-        it 'returns nil' do
-          position = [4, 2]
-          result = game.select_piece_at(position)
-          expect(result).to eq(nil)
-        end
       end
     end
   end
